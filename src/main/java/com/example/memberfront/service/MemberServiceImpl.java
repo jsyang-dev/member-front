cd8eaa593e7a0ab5a5770915536e2684c69e924c
Jeongsu Yang <mycat83@gmail.com>
2019-10-31 18:42:14 +0900

    기능: 회원정보 조회 구현
    * API 호출을 위한 RestTemplateHelper 클래스 추가
    * 회원정보 조회 관련 클래스 및 테스트 구현
    * application.yml: API 서버 정보 추가
    * spring-boot-configuration-processor 디펜던시 추가

--- src/main/java/com/example/memberfront/service/MemberServiceImpl.java
+++ src/main/java/com/example/memberfront/service/MemberServiceImpl.java
@@ -1,10 +1,29 @@
 package com.example.memberfront.service;
 
+import com.example.memberfront.common.RestTemplateHelperImpl;
 import com.example.memberfront.dto.Member;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class MemberServiceImpl implements MemberService {
+    @Value("${app.api.recipe.scheme}")
+    private String scheme;
+
+    @Value("${app.api.recipe.host}")
+    private String host;
+
+    @Value("${app.api.recipe.port}")
+    private String port;
+
+    private final RestTemplateHelperImpl restTemplateHelper;
+
+    public MemberServiceImpl(RestTemplateHelperImpl restTemplateHelper) {
+        this.restTemplateHelper = restTemplateHelper;
+    }
+
     @Override
     public void createMember(Member member) {
 
@@ -12,6 +31,14 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public Member readMember(String username) {
-        return null;
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(this.scheme)
+                .host(this.host)
+                .port(this.port)
+                .path("/member")
+                .path("/" + username)
+                .build(true);
+
+        return this.restTemplateHelper.getForEntity(Member.class, uriComponents.toUriString());
     }
 }
---
+++ src/test/java/com/example/memberfront/service/MemberServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.example.memberfront.service;
+
+import com.example.memberfront.common.RestTemplateHelperImpl;
+import com.example.memberfront.dto.Member;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MemberServiceImplTest {
+    @InjectMocks
+    private MemberServiceImpl memberService;
+
+    @Mock
+    private RestTemplateHelperImpl restTemplateHelper;
+
+    @Test
+    public void Should_정상_반환_When_회원_조회() {
+        //given
+        Member member = Member.builder()
+                .username("user1")
+                .password("1234")
+                .name("아이유")
+                .phone("01012345678")
+                .email("user1@domain.com")
+                .build();
+        given(this.restTemplateHelper.getForEntity(eq(Member.class), any(String.class))).willReturn(member);
+
+        //when
+        final Member readMember = memberService.readMember(member.getUsername());
+
+        //then
+        assertThat(readMember.getUsername(), is(readMember.getUsername()));
+        assertThat(readMember.getPassword(), is(readMember.getPassword()));
+        assertThat(readMember.getName(), is(readMember.getName()));
+        assertThat(readMember.getPhone(), is(readMember.getPhone()));
+        assertThat(readMember.getEmail(), is(readMember.getEmail()));
+    }
+}
